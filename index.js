e452400b999e3c89e429380ad7fb315bd25af72a
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-05-05 10:11:17 -0700

    Adding .addEventListener to nodes

---
+++ index.js
@@ -0,0 +1,30 @@
+//event handlers: functions that are called when events occur. 
+//JS offers 3 ways to register events:
+
+//1. Inline event listeners => <button onclick="doSomething()"></button>
+//2. DOM event handlers => window.onload = () => {//window loaded} // mostly used when handling xhr requests
+//3. Using addEventListener() => modern way and it allows us to register as many handlers as we need
+// window.addEventListener( 'load', () => { //window loaded })
+
+//Listening to events on the document itself
+document.addEventListener('click', function(event) {
+    console.log('document was clicked!')
+    console.log(event.target)
+    console.log(event.currentTarget)
+    console.log(this)
+})
+
+//Add eventListener to toxicTim when double clicked
+const toxicTim = document.getElementById("toxic-tim")
+
+toxicTim.addEventListener('dblclick', function() {
+    console.log('Toxic Tim was clicked')
+})
+
+//Exercise: Clicking Titles
+const allDoggos = document.querySelectorAll('.doggo');
+allDoggos.forEach(dog => {
+    dog.querySelector('h1').addEventListener('click', function() {
+        console.log('doggo name clicked!')
+    })
+})
