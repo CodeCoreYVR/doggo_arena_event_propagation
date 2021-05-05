b61c9613836dbe0d07e278fa3d15803197e4fa42
stephanie-warmenhoven <steph_war@yahoo.ca>
2021-05-05 10:52:39 -0700

    Added some more notes

--- index.js
+++ index.js
@@ -9,17 +9,37 @@
 //Listening to events on the document itself
 document.addEventListener('click', function(event) {
     // console.log(event)
-    // console.log('document was clicked!')
-    // console.log(event.target)
+    console.log('document was clicked!')
+    // console.log(event.target) //event.target is the node that triggered the event
 
     //Remember that currentTarget is not available to the event object other that the instance
     //when the event is triggered.  To save its value you will need to grab it like this:
     // my_currentT = event.currentTarget
     // console.log(my_currentT)
 
-    // console.log(event.currentTarget)
+    // console.log(event.currentTarget) // event.currentTarget is the node that owns the event i.e. the Node that .addEventListener was called on
     // console.log(this)
 })
+
+//Notes on currentTarget:
+//See that currentTarget cannot be used asynchronously:
+    /* See that currentTarget is null when you look at the event object's properties.  It is only available in the instance of triggering the event, 
+        and not afterwards. Event is like a live thing.  Immediately when the event happens, currentTarget exists, but as soon as the next thing happens 
+        (like console.log) it is no longer current.
+        Example, when you have console.log(event.currentTarget) => in this moment you can do something with that currentTarget
+        But if you console.log(event) after, it won’t be there anymore
+        So, if you want to use it, use it immediately and not in something asynchronous.
+        You can save the value of currentTarget by declaring a variable, though.
+    */
+
+//Notes on "this":
+    /*"this" refers to currentTarget.  However, be careful when using arrow functions with eventListeners!!
+        The value of “this” depends on the context of where you’re using it. In our current eventListener with the callback 
+        function, our “this” refers to the currentTarget.  However, if you use an arrow function, the behaviour of arrow 
+        functions means that it will bind the value of “this” to its current context.  
+        So, if you use an arrow function with addEventListener, this will not be currentTarget 
+        anymore, it’s probably going to be the node of Window.
+    */
 
 //Add eventListener to toxicTim when double clicked
 const toxicTim = document.getElementById("toxic-tim")
@@ -39,6 +59,7 @@ allDoggos.forEach(dog => {
 //To test if an object is an instance of a Node in order to prevent 
 //adding eventListener to null, use instanceof
 //document.querySelector("jgjhfgj") instanceof Node   => returns false
+//toxicTim instancof Node => returns true
 
 //Exercise: Clicks, Events and Properties
 toxicTim.addEventListener('click', function(event) {
